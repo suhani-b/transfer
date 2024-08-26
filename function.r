94e1c06db660f9c85d75e0cdb069598c30845467
suhani-b <suhani@princeton.edu>
2024-08-26 15:50:59 -0400

    prints

--- function.r
+++ function.r
@@ -63,8 +63,6 @@ test_interaction.poisson.single <- function(dat, obs,
     # dat_f = cbind(dat_f, cell_scores[rownames(dat_f),], list(GDR = gdr), adata$obs[rownames(dat_f),])
     # create metadata df
     meta <- cbind(cell_scores[rownames(dat_f), ], list(GDR = gdr), obs[rownames(dat_f), ])
-    print(meta)
-    print(rownames(dat_f))
 
     # loop through each column of factor matrix and compute interaction score
     res_i <- list()
@@ -99,6 +97,7 @@ test_interaction.poisson.single <- function(dat, obs,
     }
     
     print(dim(model_formula))
+    print(typeof(meta))
     print(dim(meta))
     print(model_formula)
     print("fitting glm")
